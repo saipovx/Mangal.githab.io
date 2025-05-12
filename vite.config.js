bdca6e9ae5207c67dc4c929e38606615c8a5b125
roma <roma.saipov@bk.ru>
2025-05-12 05:33:35 +0500

    Изменил конфигурацию Vite для деплоя на GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    outDir: 'build',  // Папка для финальной сборки
-    sourcemap: true,   // Опционально, чтобы создавать исходные карты (полезно для отладки)
+    outDir: 'docs',  // Папка для финальной сборки для GitHub Pages
+    sourcemap: true,  // Опционально, чтобы создавать исходные карты (полезно для отладки)
   },
 });
